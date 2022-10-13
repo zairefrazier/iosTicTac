b10c8793b154e2690cd5ff3462bea769c28155ef
Zaire <60757805+zairefrazier@users.noreply.github.com>
2022-10-13 03:04:59 -0400

    Moved Around Files

--- tictactoe/ContentView.swift
+++ tictactoe/ContentView.swift
@@ -7,63 +7,88 @@
 
 import SwiftUI
 
-let colums: [GridItem] = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
-
 struct ContentView: View {
     
-    @State var moves : [String] = Array(repeating: "", count: 9)
-    
-    @State var isPlaying = false
+
     var body: some View {
-//        GeometryReader { geometry in
-//            VStack {
-//                Spacer()
-//                LazyVGrid(columns: colums, spacing: 15) {
-//                    ForEach(0..<9) { i in
-//                        ZStack {
-//
-//                            RoundedRectangle(cornerRadius: 20)
-//                                .stroke(.red).opacity(1.0)
-//
-//
-//                            RoundedRectangle(cornerRadius: 20)
-//                                .foregroundColor(.blue.opacity(moves[i] == "" ? 1 : 0))
-//
-//                                .frame(width: geometry.size.width/3-15, height: geometry.size.width/3-15)
-//                            Text(moves[i])
-//                                .font(.system(size: 55))
-//                                .foregroundColor(.black)
-//                                .fontWeight(.bold)
-//
-//                        }
-//
-//                        .rotation3DEffect(.init(degrees: moves[i] != "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0), anchor: .center, anchorZ: 0.0, perspective: 1.0)
-//
-//                        .onTapGesture {
-//
-//                            withAnimation(Animation.easeIn(duration: 0.75)){
-//
-//
-//                                if moves[i] == ""{
-//                                    moves[i] = isPlaying ? "X" : "O"
-//                                    isPlaying.toggle()
-//                                }
-//
-//                            }
-//
-//                        }
-//
-//                    }
-//                }
-//                Spacer()
-//            }
-//
-//        }
-        AiTacView()
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [Color("Color"),.red], startPoint: .trailing, endPoint: .top)
+                    .ignoresSafeArea()
+                Circle()
+                    .fill(.brown)
+                    .blur(radius: 90)
+                    .offset(x: 200, y: 150)
+                                
+                VStack{
+                                    
+                    Home()
+                        .navigationTitle("Tic Tac Toe")
+                        .offset(y: -100)
+                    
+                    HStack{
+                        NavigationLink(destination: OnevOne(), label: {
+                            
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .frame(height:70)
+                                    .opacity(0.2)
+                                    .foregroundColor(.white)
+                                Text("One vs One")
+                                    .font(.title)
+                                    .bold()
+                                .foregroundColor(.white)
+                            }
+                            
+                        })
+                        Spacer()
+                        NavigationLink(destination: AiTacView() , label: {
+                            
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .frame(height:70)
+                                    .opacity(0.2)
+                                    .foregroundColor(.white)
+                                Text("One vs CPU")
+                                    .font(.title)
+                                    .bold()
+                                    .foregroundColor(.white)
+                            }
+
+                        })
+                    }
+                    .padding(.horizontal)
+                }
+            }
+        }
+        .preferredColorScheme(.dark)
+        
     }
+    
 }
 
-
+struct Home: View {
+    var body: some View {
+    
+        VStack(spacing: 20){
+            Text("Game of X's and O's!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            .multilineTextAlignment(.center)
+        }
+        .padding(.horizontal)
+        
+        VStack(spacing: 20){
+            Text("Play Tic Tac Toe!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            .multilineTextAlignment(.center)
+        }
+        .padding(.horizontal)
+    }
+}
 
 
 
---
+++ tictactoe/OnevOne.swift
@@ -0,0 +1,69 @@
+//
+//  OnevOne.swift
+//  tictactoe
+//
+//  Created by Zaire on 10/13/22.
+//
+
+import SwiftUI
+
+
+let colums: [GridItem] = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+
+struct OnevOne: View {
+    
+    @State var moves : [String] = Array(repeating: "", count: 9)
+    
+    @State var isPlaying = false
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: colums, spacing: 15) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(.red).opacity(1.0)
+
+
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundColor(.blue.opacity(moves[i] == "" ? 1 : 0))
+
+                                .frame(width: geometry.size.width/3-15, height: geometry.size.width/3-15)
+                            Text(moves[i])
+                                .font(.system(size: 55))
+                                .foregroundColor(.black)
+                                .fontWeight(.bold)
+
+                        }
+
+                        .rotation3DEffect(.init(degrees: moves[i] != "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0), anchor: .center, anchorZ: 0.0, perspective: 1.0)
+
+                        .onTapGesture {
+
+                            withAnimation(Animation.easeIn(duration: 0.75)){
+
+
+                                if moves[i] == ""{
+                                    moves[i] = isPlaying ? "X" : "O"
+                                    isPlaying.toggle()
+                                }
+
+                            }
+
+                        }
+
+                    }
+                }
+                Spacer()
+            }
+
+        }
+    }
+}
+struct OnevOne_Previews: PreviewProvider {
+    static var previews: some View {
+        OnevOne()
+    }
+}
