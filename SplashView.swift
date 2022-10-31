4828151e934d2affd0a4c48e4db21010f7904a75
Zaire <60757805+zairefrazier@users.noreply.github.com>
2022-10-31 04:53:30 -0400

    Created SplashView

---
+++ SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  tictactoe
+//
+//  Created by Zaire on 10/31/22.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
